853bed8900130a96f302ee37ab7a7af2b662fea8
Harinder <sethi.harinder@gmail.com>
2015-11-06 02:13:16 +0530

    modified tweets.py to have a fucntion

--- webapp/app/tweets.py
+++ webapp/app/tweets.py
@@ -5,21 +5,25 @@ access_token_secret = "Ad42R8zxaHq1grJcP8L8afUSve3TGOAbi7O2gdQ82oTHf"
 consumer_key = "9ff2ZYHKZhtAojUc6lBH5Nlsd"
 consumer_secret = "ElXQ20E9sjlW1X2rWVQqSto1qUikhZX6gmAt0uZF0fNHSfo2z6"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
-dates = []
-texts = []
+def thisIsSparta(sparta_tag) :
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    dates = []
+    texts = []
 
 
-tweets = tweepy.Cursor(api.search, q='#iitd').items(2)
+    tweets = tweepy.Cursor(api.search, q=sparta_tag).items(20)
 
-for tweet in tweets:
-    #print tweet.created_at, tweet.text, "\n"
-    dates.append(str(tweet.created_at))
-    texts.append(tweet.text)
+    for tweet in tweets:
+        #print tweet.created_at, tweet.text, "\n"
+        dates.append(str(tweet.created_at))
+        texts.append(tweet.text)
 
-#print dates
-#print texts
+    #print dates
+    #print texts
+
+    return dates, texts
 
