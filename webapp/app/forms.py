b2a39d676bf79e23ec848b11ddba1ded14ed8f61
abhiagar90 <abhiagar90@gmail.com>
2015-11-06 01:48:41 +0530

    basic machine learning

--- webapp/app/forms.py
+++ webapp/app/forms.py
@@ -3,16 +3,5 @@ from wtforms import StringField, BooleanField, PasswordField
 from wtforms import validators
 from wtforms.fields.html5 import EmailField
 
-class RegisterationForm(Form):
-    emailid = EmailField('Email', validators=[validators.DataRequired(), validators.Email()])
-    name = StringField('name', validators=[validators.DataRequired()])
-    password = PasswordField('password', validators=[validators.Required(), 
-        validators.EqualTo('repassword',message="Passwords must match")])
-    repassword = PasswordField('repassword', validators=[validators.Required()])
-    accept_terms = BooleanField('I accept everything!')
-
-class LoginForm(Form):
-    emailid = EmailField('Email', validators=[validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password', validators=[validators.Required()])
-    ##rem_me = BooleanField('Remember me')
-    ##TODO: some time afterwards
+class CrunchForm(Form):
+    hashtag = StringField('Hash Tag', validators=[validators.DataRequired()])
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect, session, g, request, url_for, abort
-from forms import RegisterationForm, LoginForm
-
+from monkey import *
+from forms import CrunchForm
     
 @app.route('/')
 @app.route('/home/')
@@ -9,43 +9,6 @@ from forms import RegisterationForm, LoginForm
 def home():
     return render_template("home.html", homeclass="active")
 
-@app.route('/login/',methods=["GET","POST"])
-def login():
-    if session.get('userid')>=1:
-        return render_template("temp.html", loginclass="active", signincss=False, temptext="Already logged in!")
-    form = LoginForm()
-    if form.validate_on_submit():
-        try:
-            someobj = Users.get(Users.userid == form.emailid.data, Users.password == 
-                hashlib.md5(form.password.data).hexdigest())
-            session['userid']=someobj.userid
-            session['role']=someobj.role
-            flash('Successfully logged in')
-            print session
-            return redirect('home')
-        except:
-            flash('Details do not match')
-    else:
-        form_error_helper(form) 
-    return render_template("login2.html", loginclass="active", signincss=False, form = form)
-
-@app.route('/logout/',methods=["GET","POST"])
-def logout():
-    if not session.get('userid'):
-            return render_template("temp.html", loginclass="active", signincss=False, temptext="Please log in first!")
-    session.clear()
-    return render_template("temp.html", loginclass="active", signincss=False, temptext="Successfully logged out!")
-
-#get to land first on signup page, post to actually sign up
-@app.route('/signup/', methods=["GET","POST"]) 
-def signup():
-    form = RegisterationForm()
-    if form.validate_on_submit():
-        flash('Signup details valid')
-        return redirect('home')
-    else:
-        form_error_helper(form)
-    return render_template("signup.html", signupclass="active", signincss=True, form=form) 
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -55,20 +18,29 @@ def page_not_found(e):
 def not_permitted(e):
     return render_template("error.html", homeclass="active", errortext="Sorry, you are not permitted to see this.")
 
+@app.route('/crunchresult/')
+def crunchresult():
+    reslist = getAllAnalysis(["this is awesome. i am loving this! though the time is running out. though the time is running out. though the time is running out","no","haha","never"])
+    return render_template("crunch.html", homeclass="active", temptext="See command line",
+        reslist=reslist)
+
+@app.route('/crunch/',methods=["GET","POST"])
+def crunch():
+    form = CrunchForm()
+    if form.validate_on_submit():
+        try:
+            hashtag = form.hashtag.data
+            flash('Successfully got the hash tag '+hashtag)
+            return redirect('home')
+        except:
+            flash('Error in your form submission')
+    else:
+        form_error_helper(form)
+    return render_template("crunchnow.html", crunchclass="active",signincss=False,form=form)
+
 @app.route('/temp/')
 def temp():
-    nayaperson = Person.create(name='nayaperson')
-    naya_kitty = Pet.create(ownerid=nayaperson, type='cat')
-    toflash = ''
-    for person in Person.select():
-        toflash = toflash + (str(person.id)+" "+person.name) + "\n\r"
-    flash(toflash)
-    return render_template("temp.html", homeclass="active", temptext=str(nayaperson.id)+" "
-        +str(naya_kitty.id))
-
-
-def get_current_user_role():
-    return 'admin'
+    return render_template("temp.html", homeclass="active", temptext="See command line")
 
 def form_error_helper(form):
     for field, errors in form.errors.items():
